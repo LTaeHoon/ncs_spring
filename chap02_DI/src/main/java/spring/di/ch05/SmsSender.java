99b199645a08e912167feb9331d28389c0136a95
LTaeHoon <lth1011@gmail.com>
2017-02-10 14:42:42 +0900

    spring.di.ch05 : 2개 파라미터  DI 방법(생성자 이용)

---
+++ chap02_DI/src/main/java/spring/di/ch05/SmsSender.java
@@ -0,0 +1,12 @@
+package spring.di.ch05;
+
+// 메시지 전송 클래스
+public class SmsSender {
+
+	@Override //object 클래스에서 상속받은 멤버
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "sms 메시지 전송 완료";
+	}
+	
+}
