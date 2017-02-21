3bad4113b76e546b17bbfa60a6cf3d87939eedf1
LTaeHoon <lth1011@gmail.com>
2017-02-21 17:10:42 +0900

    myBatis 예제코드 vo

---
+++ chap09_myBatis/src/main/java/vo/Product.java
@@ -0,0 +1,35 @@
+package vo;
+
+public class Product {
+	private int num;
+	private String name;
+	private int price;
+	private String reg_date;
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(String reg_date) {
+		this.reg_date = reg_date;
+	}
+	
+	
+}
